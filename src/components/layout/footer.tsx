9e4ae9a4ed879750a23105eb895f46174a3f7f05
VibeSys <vibesys.sistemas@gmail.com>
2025-05-01 09:21:52 -0300

    Delete src directory

--- src/components/layout/footer.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export function Footer() {
-  return <footer className="w-full bg-blue-dark py-3 text-center text-white mt-auto">
-      <div className="container mx-auto">
-        <p className="text-sm">©VibeSys - 2025 Direitos Reservados - Licensed to Aguia de Haia</p>
-      </div>
-    </footer>;
-}
--- src/utils/currentUser.ts
+++
@@ -1,9 +0,0 @@
-
-import { currentUser as userFromTypes } from "@/types/user";
-
-// Use the currentUser from types.ts
-export const currentUser = userFromTypes;
-
-export function getCurrentUser() {
-  return currentUser;
-}
