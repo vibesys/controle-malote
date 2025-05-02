26c6df610cf11bebab0c22a2d3656060f6660ebe
VibeSys <vibesys.sistemas@gmail.com>
2025-05-02 04:59:13 -0300

    Delete src directory

--- src/context/auth/AuthContext.ts
+++
@@ -1,8 +0,0 @@
-
-import { createContext } from 'react';
-import { AuthContextType } from './types';
-
-// Create the Auth Context with default undefined value
-const AuthContext = createContext<AuthContextType | undefined>(undefined);
-
-export default AuthContext;
--- src/utils/supabase/index.ts
+++
@@ -1,7 +0,0 @@
-
-export { empresasDB } from './empresasDB';
-export { departamentosDB } from './departamentosDB';
-export { destinatariosDB } from './destinatariosDB';
-export { meiosTransporteDB } from './meiosTransporteDB';
-export { malotesDB } from './malotesDB';
-export { logsDB } from './logsDB';
